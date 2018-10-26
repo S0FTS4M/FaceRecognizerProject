b10099c1cd526878b75a015bb13666462bee9ede
SOFTSAM <sml.ozclk@gmail.com>
2018-10-26 16:35:56 +0300

    comments added and edited
    TODO
    *After training save trained file and load
    *Connect external network camera with ip
    *Guessing FPS is 16 for right now, make improvements about that
    *Train from original images not only from faces.

--- FaceRecognizerProject/Form1.cs
+++ FaceRecognizerProject/Form1.cs
@@ -15,14 +15,30 @@ namespace FaceRecognizerProject
     {
         //Capture will access the camera and reads the stream
         private static CascadeClassifier classifier = new CascadeClassifier(@"haarcascade_frontalface_alt_tree.xml");
-        //gets camera stream as images
-        Capture capture;
-        //The original image we read from cam
-        Image<Bgr, byte> capturedImage = null;
+        
         //Recognizer Algorithm
         FaceRecognizer faceRecognizer = new LBPHFaceRecognizer(1, 8, 8, 8, 100.0);
         //The text font that we write the person name
         MCvFont mCvFont = new MCvFont(FONT.CV_FONT_HERSHEY_TRIPLEX, 1.0, 1.0);
+        
+
+        //random numbers for file names
+        Random random = new Random();
+        
+        //FPS Counter
+        Stopwatch stopwatch = new Stopwatch();
+        int fps = 0;
+
+        /// <summary>
+        /// Capture Vars
+        /// </summary>
+        //gets camera stream as images
+        Capture capture;
+        //The original image we read from cam
+        Image<Bgr, byte> capturedImage = null;
+        //capturing info
+        Color foreColor;
+        string info = "";
         //if you dont want to try catching everytime your face detected you an enable auto capture
         bool autoCapture = false;
         //there will be a limit of time that you  can auto capture otherwise you will have a large amount of same face
@@ -30,29 +46,25 @@ namespace FaceRecognizerProject
         //all settings are ready for capturing face? like does this person have a name
         private bool canCapture = false;
 
-
-        Random random = new Random();
-        //FPS Counter
-        Stopwatch stopwatch = new Stopwatch();
-        int fps = 0;
-
-
-        Color foreColor;
-        string info = "";
-
+        //training phase
+        //TODO: Save these names with recognizer and load after
         List<string> listOfFileNames = new List<string>();
         bool modeltrained = false;
         public frmfacerec()
         {
             InitializeComponent();
             capture = new Capture();
+            //start timer and start capturing images
             timerCameraFramer.Start();
+            //for fps counting
             stopwatch.Start();
         }
 
         private void timerCameraFramer_Tick(object sender, EventArgs e)
         {
+            //reset canCapture every frame
             canCapture = false;
+         ///FPS counter start
             fps++;
             if (stopwatch.ElapsedMilliseconds >= 1000)
             {
@@ -61,14 +73,19 @@ namespace FaceRecognizerProject
                 fps = 0;
                 stopwatch.Restart();
             }
+            ///fps counter end
+            
+            ///read from cam and get the 3 channel image
             capturedImage = capture.QueryFrame();
+            //if we didnt train the model we just can detect the face
             if (modeltrained == false)
             {
-                Tuple<Image<Bgr, byte>, Image<Gray, byte>> faceRectandGrayFace = DetecetFace(capturedImage.Clone());
+                //get Detected face and detected face rectangle
+                Tuple<Image<Bgr, byte>, Image<Gray, byte>> faceRectandGrayFace = DetectedFace(capturedImage.Clone());
                 Image<Bgr, byte> foundFaceRect = faceRectandGrayFace.Item1;
                 Image<Gray, byte> foundFace = faceRectandGrayFace.Item2;
 
-
+                //did we captured successfully or not
                 info = "";
                 if (autoCapture && txtpersonname.Text != string.Empty)
                 {
@@ -79,8 +96,11 @@ namespace FaceRecognizerProject
                         if (canCapture)
                         {
 
+                            //save image with name and 3 other random numbers
                             SaveImage(foundFace, txtpersonname.Text + random.Next(0, 9).ToString() + random.Next(0, 9).ToString() + random.Next(0, 9).ToString());
+                            //reset elapsed time
                             elapsedTimeforAutoCapture = 0;
+                            //saving is successfull
                             info = "Image Captured";
                             foreColor = Color.Green;
                             //make white all the pixels for flash light :D
@@ -88,25 +108,31 @@ namespace FaceRecognizerProject
                         }
                         else
                         {
+                            //if we are here we couldnt capture the image so we dont want to wait another X ms
+                            //so we can go 100ms back
                             elapsedTimeforAutoCapture -= 100;
+                            //saving is failed
                             info = "Image could not captured";
                             foreColor = Color.Red;
                         }
 
                     }
                 }
+                //if we can capture but there is no person name we fail saving the faces
                 else if (canCapture && txtpersonname.Text == string.Empty)
                 {
                     elapsedTimeforAutoCapture -= 100;
                     info = "Image could not captured";
                     foreColor = Color.Red;
                 }
+                //show capturing info
                 lblCapturingInfo.Text = info;
                 lblCapturingInfo.ForeColor = foreColor;
-
+                
+                //show the image with rectangled face on it
                 imageBLiveCamera.Image = foundFaceRect;
             }
-            //if model trained then we need to guess
+            //if model trained then we need to predict who is it
             if (modeltrained == true)
             {
                 Guess(capturedImage);
@@ -115,13 +141,16 @@ namespace FaceRecognizerProject
 
         private void Guess(Image<Bgr, byte> capturedImage)
         {
+            //we need the gray scale image so we apply convert to gray
             Image<Gray, byte> grayImage = capturedImage.Copy().Convert<Gray, byte>();
+            //detects every face in an image
             var faces = classifier.DetectMultiScale(
                 grayImage,
                 1.3,
                 5,
                 new Size(64, 64),
                 Size.Empty);
+            //for every rectangle that we found from classifier.DetectMultiScale we need to make a guess
             foreach (var rect in faces)
             {
                 PredictionResult result;
@@ -131,31 +160,38 @@ namespace FaceRecognizerProject
                 }
                 catch
                 {
+                    //if any exception happens we dont want to use this so we give it a -1 label
                     result.Label = -1;
                 }
                 if (result.Label >= 0)
                 {
+                    //draw the face rectangle
                     capturedImage.Draw(rect, new Bgr(Color.OrangeRed), 3);
-
+                    //draw the filled rectangle under the face rectangle
                     capturedImage.Draw(new Rectangle(new Point(rect.X, rect.Y + rect.Height), new Size(rect.Width - 2, 20)), new Bgr(Color.OrangeRed), 20);
+                    //draw prediction result over filled rectangle
                     capturedImage.Draw(listOfFileNames[result.Label], ref mCvFont, new Point(rect.X + 4, rect.Y + rect.Height + 15), new Bgr(Color.White));
                 }
                 else
                 {
-                    capturedImage.Draw(rect, new Bgr(Color.Red), 4);
-                    capturedImage.Draw("Unknown", ref mCvFont, new Point(rect.X - 2, rect.Y - 2), new Bgr(Color.White));
+                    capturedImage.Draw(rect, new Bgr(Color.Red), 3);
+                    //draw the filled rectangle under the face rectangle
+                    capturedImage.Draw(new Rectangle(new Point(rect.X, rect.Y + rect.Height), new Size(rect.Width - 2, 20)), new Bgr(Color.Red), 20);
+                    capturedImage.Draw("Unknown", ref mCvFont, new Point(rect.X + 4, rect.Y + rect.Height + 15), new Bgr(Color.White));
                 }
             }
             imageBLiveCamera.Image = capturedImage;
         }
 
-        private Tuple<Image<Bgr, byte>, Image<Gray, byte>> DetecetFace(Image<Bgr, byte> _image)
+        private Tuple<Image<Bgr, byte>, Image<Gray, byte>> DetectedFace(Image<Bgr, byte> _image)
         {
             if (_image == null)
                 return null;
 
+            //convert image to gray scale
             Image<Gray, byte> grayImage = _image.Convert<Gray, byte>();
             Image<Gray, byte> face = null;
+            //detect faces 
             var faces = classifier.DetectMultiScale(
                 grayImage,
                 1.3,
@@ -163,6 +199,7 @@ namespace FaceRecognizerProject
                 new Size(64, 64),
                 Size.Empty
                 );
+            //draw rectangles for every face
             foreach (var rect in faces)
             {
                 _image.Draw(rect, new Bgr(Color.Green), 2);
@@ -170,7 +207,7 @@ namespace FaceRecognizerProject
                 canCapture = true;
                 face = grayImage.Copy(rect);
             }
-
+            //return rectabgled image and gray scaled face image
             return new Tuple<Image<Bgr, byte>, Image<Gray, byte>>(_image, face);
         }
 
@@ -200,16 +237,23 @@ namespace FaceRecognizerProject
 
         private void btnTrain_Click(object sender, EventArgs e)
         {
+            //select a training folder with faces
             DialogResult dialogResult = folderBrowserTraininImages.ShowDialog();
-            List<Image<Gray, byte>> images = new List<Image<Gray, byte>>();
+            //list of faces
+            List<Image<Gray, byte>> faceImagesList = new List<Image<Gray, byte>>();
+            //list of labels
             List<int> labels = new List<int>();
             if (dialogResult == DialogResult.OK)
             {
+                //i is the index of the person if we are training with same person so we dont need to increase i
+                //so we need another index storage variable
                 int i = 0, lastPersonindex = 0;
+                //get every file from specified directory if you want to select from folderBrowser
+                //folderBrowserTraininImages.SelectedPath add this code inside of GetFiles()
                 foreach (string file in Directory.GetFiles(Application.StartupPath + "\\training\\"))
                 {
                     Image<Gray, byte> faceImage = new Image<Gray, byte>(file);
-                    images.Add(faceImage);
+                    faceImagesList.Add(faceImage);
 
                     string name = Path.GetFileName(file).Split('.')[0];
                     name = name.Substring(0, name.Length - 3);
@@ -223,7 +267,7 @@ namespace FaceRecognizerProject
                     else
                         labels.Add(lastPersonindex);
                 }
-                faceRecognizer.Train(images.ToArray(), labels.ToArray());
+                faceRecognizer.Train(faceImagesList.ToArray(), labels.ToArray());
                 gruopTrained.Enabled = modeltrained = true;
                 listBox1.Items.AddRange(listOfFileNames.ToArray());
             }
@@ -244,7 +288,7 @@ namespace FaceRecognizerProject
                 return;
             }
             
-            Image<Bgr, byte> foundFace = DetecetFace(capturedImage.Clone()).Item1;
+            Image<Bgr, byte> foundFace = DetectedFace(capturedImage.Clone()).Item1;
             if (canCapture)
             {
                 info = "Image Captured";
